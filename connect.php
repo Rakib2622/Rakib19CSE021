17364b249eb7e82ef74a95e2171cfcb4f9c41a7f
Rakib2622 <60670376+Rakib2622@users.noreply.github.com>
2022-10-10 00:02:38 +0600

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,9 @@
+<?php
+
+// Create connection
+// $conn = new mysqli('localhost', 'id19540198_root', 'Na]Z8e?\RUG^JYa^', 'id19540198_crud_project');
+//$conn = mysqli_connect("localhost","id19684800_root","lA(L?yzpT[kh5i?P","id19684800_product");
+$conn = mysqli_connect("localhost","root","","product");
+
+
+?>
